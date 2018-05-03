b15d9bbd0438cea41d665ee0b67dd1eedff64609
Denise Pendleton <dpendl1@hotmail.com>
2018-05-03 21:18:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,11 +49,11 @@ end
  end
 
  get '/tweets/:id' do
-   @user = User.find(session[:user_id])
-    if current_user == @user
+  
    @tweet = Tweet.find(params[:id])
    @user = User.find(session[:user_id])
-   @user.id = @tweet.user_id
+   if @user.id = @tweet.user_id
+   
    erb :"tweets/show"
  elsif logged_in?
    redirect '/tweets'
