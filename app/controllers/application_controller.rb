686eedef06b5c5eea91109ff6e6cada3a2308500
Denise Pendleton <dpendl1@hotmail.com>
2018-05-03 15:19:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -82,7 +82,7 @@ end
 post "/login" do
     # user = User.find_by(username: params[:username])
     # if user && user.authenticate(params[:password])
-    logged_in?
+    if logged_in?
           session[:user_id] = user.id
           session[:email] = params[:email]
           session[:username] = params[:username]
