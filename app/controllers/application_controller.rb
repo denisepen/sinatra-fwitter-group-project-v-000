b1bd109b7027c95b02e22a2dbb1fb826199fb482
Denise Pendleton <dpendl1@hotmail.com>
2018-05-03 23:59:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -95,17 +95,17 @@ patch '/tweets/:id' do
   # binding.pry
   if logged_in?
    @tweet=Tweet.find(params[:id])
-   if !params[:content].empty?
-   @tweet.update(content: params[:content])
-  # @tweet.content = params[:tweet]
-  session[:tweet] = params[:content]
-  @user = User.find(session[:user_id])
-  @user.id = @tweet.user_id
-  @tweet.save
-  redirect "/tweets/#{@tweet.id}"
-else
-  redirect "/tweets/#{@tweet.id}/edit"
-end
+     if !params[:content].empty?
+     @tweet.update(content: params[:content])
+    # @tweet.content = params[:tweet]
+    session[:tweet] = params[:content]
+    @user = User.find(session[:user_id])
+    @user.id = @tweet.user_id
+    @tweet.save
+    redirect "/tweets/#{@tweet.id}"
+  else
+    redirect "/tweets/#{@tweet.id}/edit"
+  end
 else
   redirect '/login'
 end
