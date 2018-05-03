74f866c77358e687c290398db4fd09e37ea371a1
Denise Pendleton <dpendl1@hotmail.com>
2018-05-03 13:46:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,14 +51,15 @@ end
 post '/signup' do
 
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
-       redirect "/signup"
+         redirect "/signup"
      else
-       @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-       @user.save
+       
       #  binding.pry
        session[:user_id] = @user.id
        session[:email] = params[:email]
        session[:username] = params[:username]
+       @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+       @user.save
        redirect '/tweets'
     end
 end
