b92315c8ac4f06b2fe74e502451b27c2f1f8dbb2
Denise Pendleton <dpendl1@hotmail.com>
2018-05-03 15:01:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,6 +46,7 @@ end
 
 post '/show' do
   @user=User.find(session[:user_id])
+  @tweet = Tweet.new(content: params[:tweet])
   # @user.tweets.content = params[:tweet]
     binding.pry
   @user.tweets << @tweet
