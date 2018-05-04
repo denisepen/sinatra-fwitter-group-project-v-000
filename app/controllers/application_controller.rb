2ddb713f46db6a9b0e0a224ae5f9a25297f170a1
Denise Pendleton <dpendl1@hotmail.com>
2018-05-04 00:30:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -162,8 +162,8 @@ delete '/tweets/:id/delete' do
  # @user = User.find(session[:id])
    if logged_in? #&& @user
   @tweet = Tweet.find_by_id(params[:id])
-  @user = User.find(session[:id])
-    if @tweet.user_id == @user.id
+  # @user = 
+    if @tweet.user_id == User.find(session[:id]).id
     @tweet.delete
     redirect '/tweets'
     end
