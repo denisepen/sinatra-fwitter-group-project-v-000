95071346f74573c494b9335a9f5902daad6aad29
Denise Pendleton <dpendl1@hotmail.com>
2018-05-04 00:48:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -57,7 +57,8 @@ end
 
      @user = User.find(session[:user_id])
    erb :"tweets/show"
- elsif logged_in? #&& session[:user_id] != @tweet.user_id
+ elsif logged_in? && session[:user_id] != @tweet.user_id
+   @user = User.find(session[:user_id])
    erb :"tweets/show"
   #  redirect '/tweets'
  else
