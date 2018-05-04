516f48c96eed85fa5aa6ef43185a290a0fde7dbf
Denise Pendleton <dpendl1@hotmail.com>
2018-05-04 01:53:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -162,8 +162,8 @@ post "/login" do
 end
 
 delete '/tweets/:id/delete' do
-  @user = User.find(session[:id])
-   if logged_in? && @user
+  # @user = User.find(session[:id])
+   if logged_in? #&& @user
   @tweet = Tweet.find_by_id(params[:id])
   # @user =
     #  if @tweet.user_id == User.find(session[:id]).id
